d1b9c53b5ad597bdbb912de29bf1d28f2a942427
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-10-13 21:50:58 +0200

    Add the tensor type

--- unmarshal_test.go
+++ unmarshal_test.go
@@ -36,9 +36,9 @@ func TestUnmarshalAttributes(t *testing.T) {
 		}
 	}
 	type validReceiverType struct {
-		I    int64          `attributeName:"I" required:"true"`
-		Ints []int64        `attributeName:"INTS" required:"true"`
-		T    *tensor.Tensor `attributeName:"Tensor" required:"true"`
+		I    int64         `attributeName:"I" required:"true"`
+		Ints []int64       `attributeName:"INTS" required:"true"`
+		T    tensor.Tensor `attributeName:"Tensor" required:"true"`
 	}
 	var validReceiver validReceiverType
 	err = UnmarshalAttributes(nil, &validReceiver)
@@ -51,6 +51,27 @@ func TestUnmarshalAttributes(t *testing.T) {
 	simpleAttributeINTSName := "INTS"
 	simpleAttributeINTSType := AttributeProto_INTS
 	simpleAttributeINTSValue := []int64{1, 2, 3, 4}
+	simpleAttributeTensorName := "Tensor"
+	simpleAttributeTensorType := AttributeProto_TENSOR
+	dims := []int64{8, 1, 1}
+	dataType := TensorProto_DataType(1)
+	floatData := []float32{-0.1615397185087204, -0.4338356554508209, 0.09164135903120041, -0.01685221679508686, -0.06502643972635269, -0.1317378729581833, 0.020417550578713417, -0.1211102306842804}
+	name := "testFloat"
+	simpleAttributeTensorValue := &TensorProto{
+		Dims:       dims,
+		DataType:   &dataType,
+		Segment:    (*TensorProto_Segment)(nil),
+		FloatData:  floatData,
+		Int32Data:  nil,
+		StringData: nil,
+		Int64Data:  nil,
+		Name:       &name,
+		DocString:  nil,
+		RawData:    nil,
+		DoubleData: nil,
+		Uint64Data: nil,
+	}
+
 	simpleAttributes := []AttributeProto{
 		AttributeProto{
 			Name: &simpleAttributeIName,
@@ -61,6 +82,11 @@ func TestUnmarshalAttributes(t *testing.T) {
 			Name: &simpleAttributeINTSName,
 			Type: &simpleAttributeINTSType,
 			Ints: simpleAttributeINTSValue,
+		},
+		AttributeProto{
+			Name: &simpleAttributeTensorName,
+			Type: &simpleAttributeTensorType,
+			T:    simpleAttributeTensorValue,
 		},
 	}
 	err = UnmarshalAttributes(simpleAttributes, &validReceiver)
@@ -78,4 +104,5 @@ func TestUnmarshalAttributes(t *testing.T) {
 			t.FailNow()
 		}
 	}
+	t.Log(validReceiver)
 }
