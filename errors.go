7358574b5cfde928754223c1b641914dbe1689e8
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-10-15 11:13:59 +0200

    fix(err): specific message for attributes

--- errors.go
+++ errors.go
@@ -1,6 +1,10 @@
 package onnx
 
-import "github.com/pkg/errors"
+import (
+	fmt "fmt"
+
+	"github.com/pkg/errors"
+)
 
 var (
 	// ErrNotYetImplemented ...
@@ -14,11 +18,18 @@ var (
 // ErrNotImplemented is returned for any operator or attribute
 type ErrNotImplemented struct {
 	Operator       string
-	AttributeName  *string
+	AttributeName  string
 	AttributeValue interface{}
 	Message        string
 }
 
 func (e *ErrNotImplemented) Error() string {
+	if e.AttributeName != "" {
+		return fmt.Sprintf("onnx: operator %v. Implementation error for attribute %v (%#v): %v",
+			e.Operator,
+			e.AttributeName,
+			e.AttributeValue,
+			e.Message)
+	}
 	return "onnx: operator " + e.Operator + " not implemented"
 }
