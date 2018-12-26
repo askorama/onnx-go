c492f43482283eb6a3023cccb558cf42d6d725d5
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-12-26 20:52:16 +0100

    WIP

--- graph_decoder.go
+++ graph_decoder.go
@@ -2,12 +2,13 @@ package onnx
 
 import (
 	pb "github.com/owulveryck/onnx-go/internal/pb-onnx"
-	"gonum.org/v1/gonum/graph/encoding"
+	"gonum.org/v1/gonum/graph"
 )
 
 // Unmarshal onnx encoded model proto data into a graph builder
-func Unmarshal(data []byte, dst encoding.Builder) error {
+func Unmarshal(data []byte, dst graph.DirectedBuilder) error {
 	model := new(pb.ModelProto)
 	err := model.Unmarshal(data)
+
 	return err
 }
