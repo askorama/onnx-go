96d1f3ecbc068948c8bfe1408637ec5318acd0d8
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-01-02 17:40:36 +0100

    WIP

--- simple/graph.go
+++ simple/graph.go
@@ -1,4 +1,4 @@
-package main
+package simple
 
 import (
 	"fmt"
@@ -11,11 +11,12 @@ import (
 	"gorgonia.org/tensor"
 )
 
-type simpleGraph struct {
+// Graph is a simple directed graph
+type Graph struct {
 	g *simple.WeightedDirectedGraph
 }
 
-type node struct {
+type Node struct {
 	id          int64
 	name        string
 	description string
@@ -25,12 +26,12 @@ type node struct {
 }
 
 // ID to fulfil the graph.Node interface
-func (n *node) ID() int64 {
+func (n *Node) ID() int64 {
 	return n.id
 }
 
 // Attributes it a method to fulfil the encoding/dot package
-func (n *node) Attributes() []encoding.Attribute {
+func (n *Node) Attributes() []encoding.Attribute {
 	var value string
 	value = fmt.Sprintf(`%v`, n.name)
 	if n.opType != "" {
@@ -53,44 +54,44 @@ func (n *node) Attributes() []encoding.Attribute {
 }
 
 // SetDoc to fulfil the Docuemnter interface
-func (n *node) SetDoc(name string) {
+func (n *Node) SetDoc(name string) {
 	n.description = name
 }
 
 // SetName to fulfil the Namer interface
-func (n *node) SetName(desc string) {
+func (n *Node) SetName(desc string) {
 	n.name = desc
 }
 
 // GetName to fulfil the Namer interface
-func (n *node) GetName() string {
+func (n *Node) GetName() string {
 	return n.name
 }
 
 // SetDoc to fulfil the Namer interface
-func (n *node) SetDescription(desc string) {
+func (n *Node) SetDescription(desc string) {
 	n.description = desc
 }
 
 // GetDoc to fulfil the Namer interface
-func (n *node) GetDescription() string {
+func (n *Node) GetDescription() string {
 	return n.description
 }
 
-func (n *node) SetValue(t tensor.Tensor) error {
+func (n *Node) SetValue(t tensor.Tensor) error {
 	n.value = t
 	return nil
 }
 
-func (n *node) GetValue() tensor.Tensor {
+func (n *Node) GetValue() tensor.Tensor {
 	return n.value
 }
 
-func (n *node) SetOpType(op string) {
+func (n *Node) SetOpType(op string) {
 	n.opType = op
 }
 
-func (n *node) SetOpAttributes(attrs []*onnx.Attribute) error {
+func (n *Node) SetOpAttributes(attrs []*onnx.Attribute) error {
 	n.attributes = attrs
 	return nil
 }
@@ -99,7 +100,8 @@ type attributer []encoding.Attribute
 
 func (a attributer) Attributes() []encoding.Attribute { return a }
 
-func (g *simpleGraph) DOTAttributers() (graph, node, edge encoding.Attributer) {
+// DOTAttributers is used for representation in graphviz
+func (g *Graph) DOTAttributers() (graph, node, edge encoding.Attributer) {
 	graphAttributes := attributer{
 		encoding.Attribute{
 			Key:   "label",
@@ -127,57 +129,55 @@ func (g *simpleGraph) DOTAttributers() (graph, node, edge encoding.Attributer) {
 	return graphAttributes, nodeAttributes, attributer{}
 }
 
-// SetDoc to fulfil the Documenter interface
-
-func newSimpleGraph() *simpleGraph {
-	return &simpleGraph{
+func NewSimpleGraph() *Graph {
+	return &Graph{
 		g: simple.NewWeightedDirectedGraph(math.MaxFloat64, -1),
 	}
 }
 
-func (g *simpleGraph) SetWeightedEdge(e graph.WeightedEdge) {
+func (g *Graph) SetWeightedEdge(e graph.WeightedEdge) {
 	g.g.SetWeightedEdge(e)
 
 }
-func (g *simpleGraph) NewWeightedEdge(from, to graph.Node, w float64) graph.WeightedEdge {
+func (g *Graph) NewWeightedEdge(from, to graph.Node, w float64) graph.WeightedEdge {
 	return g.g.NewWeightedEdge(from, to, w)
 
 }
-func (g *simpleGraph) AddNode(n graph.Node) {
+func (g *Graph) AddNode(n graph.Node) {
 	g.g.AddNode(n)
 
 }
-func (g *simpleGraph) NewNode() graph.Node {
+func (g *Graph) NewNode() graph.Node {
 	n := g.g.NewNode()
-	return &node{
+	return &Node{
 		id: n.ID(),
 	}
 }
 
-func (g *simpleGraph) Node(id int64) graph.Node {
+func (g *Graph) Node(id int64) graph.Node {
 	return g.g.Node(id)
 }
 
-func (g *simpleGraph) Nodes() graph.Nodes {
+func (g *Graph) Nodes() graph.Nodes {
 	return g.g.Nodes()
 }
 
-func (g *simpleGraph) From(id int64) graph.Nodes {
+func (g *Graph) From(id int64) graph.Nodes {
 	return g.g.From(id)
 }
 
-func (g *simpleGraph) HasEdgeBetween(xid, yid int64) bool {
+func (g *Graph) HasEdgeBetween(xid, yid int64) bool {
 	return g.g.HasEdgeBetween(xid, yid)
 }
 
-func (g *simpleGraph) Edge(uid, vid int64) graph.Edge {
+func (g *Graph) Edge(uid, vid int64) graph.Edge {
 	return g.g.Edge(uid, vid)
 }
 
-func (g *simpleGraph) HasEdgeFromTo(uid, vid int64) bool {
+func (g *Graph) HasEdgeFromTo(uid, vid int64) bool {
 	return g.g.HasEdgeFromTo(uid, vid)
 }
 
-func (g *simpleGraph) To(id int64) graph.Nodes {
+func (g *Graph) To(id int64) graph.Nodes {
 	return g.g.To(id)
 }
