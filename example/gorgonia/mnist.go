9653ab9e7de8db127a2184b12f55d73190e4ef0b
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-02-01 15:36:11 +0100

    chore

--- example/gorgonia/mnist.go
+++ example/gorgonia/mnist.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"os"
 
+	"github.com/gogo/protobuf/proto"
 	onnx "github.com/owulveryck/onnx-go"
 	"github.com/owulveryck/onnx-go/internal/examples/mnist"
 	pb "github.com/owulveryck/onnx-go/internal/pb-onnx"
@@ -16,7 +19,8 @@ func main() {
 
 	graph := gorgonnx.NewGraph()
 	m := onnx.NewModel(graph)
-	err := m.Unmarshal(mnist.GetMnist())
+	b := mnist.GetMnist()
+	err := m.Unmarshal(b)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -26,7 +30,7 @@ func main() {
 	}
 
 	sampleTestData := new(pb.TensorProto)
-	err = sampleTestData.XXX_Unmarshal(mnist.GetInput0())
+	err = sampleTestData.XXX_Unmarshal(mnist.GetTest1Input0())
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -51,7 +55,61 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, v := range m.Output {
-		fmt.Println(graph.Node(v).(node.Node).Value().Data())
+	/*
+		for _, v := range m.Output {
+			fmt.Println(graph.Node(v).(node.Node).Value().Data())
+		}
+	*/
+	// DEBUGGING
+	mp := &pb.ModelProto{}
+	err = proto.Unmarshal(b, mp)
+	if err != nil {
+		log.Fatal(err)
 	}
+	w := os.Stdout
+	fmt.Fprintf(w, "const dir = \"numpy/\"\n")
+	for i, nodeProto := range mp.Graph.Node {
+		fmt.Fprintf(w, "func Test%v%v(t *testing.T) {\n", nodeProto.OpType, i)
+		fmt.Fprintf(w, "save := dir + \"%v%v\"\n", nodeProto.OpType, i)
+		fmt.Fprintf(w, "os.MkdirAll(save, os.ModePerm)\n")
+		// Input
+		for i := range nodeProto.Input {
+			n, ok := m.GetNodeByName(nodeProto.Input[i])
+			if !ok {
+				log.Fatalf("Node %v not found", nodeProto.Input[i])
+			}
+			//fmt.Printf("[%v] Input %v: %v\n", nodeProto.OpType, i, n.(node.Node).Value().Dtype())
+			writeTensorTo(w, fmt.Sprintf("input%v", i), n.(node.Node))
+		}
+		// Output
+		if len(nodeProto.Output) != 1 {
+			log.Fatal("Weird")
+		}
+		n, ok := m.GetNodeByName(nodeProto.Output[0])
+		if !ok {
+			log.Fatalf("Node %v not found", nodeProto.Output[0])
+		}
+		writeTensorTo(w, "output", n.(node.Node))
+		//fmt.Printf("[%v] Output: %v\n", nodeProto.OpType, n.(node.Node).Value())
+		fmt.Fprintf(w, "}\n")
+	}
+}
+
+func writeTensorTo(w io.Writer, name string, n node.Node) error {
+	fmt.Fprintf(w, "f%v, err := os.Create(save + \"/%v.np\")\n", name, name)
+	fmt.Fprintf(w, "if err != nil {\n")
+	fmt.Fprintf(w, "t.Fatal(err)\n")
+	fmt.Fprintf(w, "}\n")
+	fmt.Fprintf(w, "defer f%v.Close()\n", name)
+	fmt.Fprintf(w, "%v := tensor.New(\n", name)
+	fmt.Fprintf(w, "\ttensor.WithShape%v,\n", n.Value().Shape())
+	fmt.Fprintf(w, "\ttensor.Of(tensor.Dtype{reflect.TypeOf(%v(1))}),\n", n.Value().Dtype())
+	fmt.Fprintf(w, "\ttensor.WithBacking(%#v),\n", n.Value().Data())
+	fmt.Fprintf(w, ")\n")
+	fmt.Fprintf(w, "err = %v.WriteNpy(f%v)\n", name, name)
+	fmt.Fprintf(w, "if err != nil {\n")
+	fmt.Fprintf(w, "t.Fatal(err)\n")
+	fmt.Fprintf(w, "}\n")
+
+	return nil
 }
