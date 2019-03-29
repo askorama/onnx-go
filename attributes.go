60ad4e1d1ba4befd3d8aabdd7d17fe4ce083a981
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-03-29 21:29:35 +0100

    feat: add a prvate func to turn pb.AttributeProto to map[string]interface{}

--- attributes.go
+++ attributes.go
@@ -4,32 +4,55 @@ import (
 	pb "github.com/owulveryck/onnx-go/internal/pb-onnx"
 )
 
-// UnmarshalAttributes reads the array of attributes and stores the result in the struct pointed to by v. If v is nil or not a pointer to a struct, Unmarshal returns an InvalidUnmarshalError.
-// The structure pointed by v can only be flat and composed of one of the following types:
-//    * string
-//    * []string
-//    * int64
-//    * []int64
-//    * float32
-//    * []float32
-//    * tensor.Tensor
-//
-// The values are associated thanks to the `onnx` tag fields and `required` tag if needed.
-// Warning: any attribute not present in the v structure is silently discarded
-func unmarshalAttributes(attrs []*pb.AttributeProto, v interface{}) error {
-	if _, ok := v.(AttributesUnmarshaler); ok {
-		return v.(AttributesUnmarshaler).UnmarshalONNXAttributes(toOperationAttributes(attrs))
-	}
-	return pb.UnmarshalAttributes(attrs, v)
-}
-
-// TODO ...
 func toOperationAttributes(attrs []*pb.AttributeProto) map[string]interface{} {
-	return nil
-}
-
-// AttributesUnmarshaler is the interface implemented by types that can unmarshal  to
-// themselves. AttributesUnmarshaler must copy the data if it wished to retain the data after returning.
-type AttributesUnmarshaler interface {
-	UnmarshalONNXAttributes(map[string]interface{}) error
+	output := make(map[string]interface{}, len(attrs))
+	for _, attr := range attrs {
+		switch attr.GetType() {
+		case pb.AttributeProto_UNDEFINED:
+			output[attr.Name] = struct{}{}
+		case pb.AttributeProto_FLOAT:
+			output[attr.Name] = attr.GetF()
+		case pb.AttributeProto_INT:
+			output[attr.Name] = attr.GetI()
+		case pb.AttributeProto_STRING:
+			output[attr.Name] = string(attr.GetS())
+		case pb.AttributeProto_TENSOR:
+			t, err := attr.GetT().Tensor()
+			if err != nil {
+				panic(err)
+			}
+			output[attr.Name] = t
+		case pb.AttributeProto_GRAPH:
+			panic(&ErrNotImplemented{
+				AttributeName:  attr.Name,
+				AttributeValue: attr,
+				Message:        "pb.AttributeProto_GRAPH not handled yet",
+			})
+		case pb.AttributeProto_FLOATS:
+			output[attr.Name] = attr.GetFloats()
+		case pb.AttributeProto_INTS:
+			output[attr.Name] = attr.GetInts()
+		case pb.AttributeProto_STRINGS:
+			output[attr.Name] = attr.GetFloats()
+		case pb.AttributeProto_TENSORS:
+			panic(&ErrNotImplemented{
+				AttributeName:  attr.Name,
+				AttributeValue: attr,
+				Message:        "pb.AttributeProto_TENSORS not handled yet",
+			})
+		case pb.AttributeProto_GRAPHS:
+			panic(&ErrNotImplemented{
+				AttributeName:  attr.Name,
+				AttributeValue: attr,
+				Message:        "pb.AttributeProto_GRAPHS not handled yet",
+			})
+		default:
+			panic(&ErrNotImplemented{
+				AttributeName:  attr.Name,
+				AttributeValue: attr,
+				Message:        "undefined attributeproto type",
+			})
+		}
+	}
+	return output
 }
