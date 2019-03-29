bb53262316e3a1b6b7922c7fd54f5eed42cbb662
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-03-29 11:24:00 +0100

    WIP: New version of UnmarshalONNXAttributes (cf https://github.com/owulveryck/onnx-go/issues/8#issuecomment-477946294)

--- attributes.go
+++ attributes.go
@@ -1,6 +1,8 @@
 package onnx
 
-import pb "github.com/owulveryck/onnx-go/internal/pb-onnx"
+import (
+	pb "github.com/owulveryck/onnx-go/internal/pb-onnx"
+)
 
 // UnmarshalAttributes reads the array of attributes and stores the result in the struct pointed to by v. If v is nil or not a pointer to a struct, Unmarshal returns an InvalidUnmarshalError.
 // The structure pointed by v can only be flat and composed of one of the following types:
@@ -14,6 +16,20 @@ import pb "github.com/owulveryck/onnx-go/internal/pb-onnx"
 //
 // The values are associated thanks to the `onnx` tag fields and `required` tag if needed.
 // Warning: any attribute not present in the v structure is silently discarded
-func UnmarshalAttributes(attrs []*pb.AttributeProto, v interface{}) error {
+func unmarshalAttributes(attrs []*pb.AttributeProto, v interface{}) error {
+	if _, ok := v.(AttributesUnmarshaler); ok {
+		return v.(AttributesUnmarshaler).UnmarshalONNXAttributes(toOperationAttributes(attrs))
+	}
 	return pb.UnmarshalAttributes(attrs, v)
+}
+
+// TODO ...
+func toOperationAttributes(attrs []*pb.AttributeProto) map[string]interface{} {
+	return nil
+}
+
+// AttributesUnmarshaler is the interface implemented by types that can unmarshal  to
+// themselves. AttributesUnmarshaler must copy the data if it wished to retain the data after returning.
+type AttributesUnmarshaler interface {
+	UnmarshalONNXAttributes(map[string]interface{}) error
 }
