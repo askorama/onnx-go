bd6418dbc40f14e9812bbbdcf54e26910d541637
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-04-14 21:17:00 +0200

    run tests in parallel

--- backend/x/gorgonnx/onnx_test.go
+++ backend/x/gorgonnx/onnx_test.go
@@ -10,9 +10,10 @@ import (
 // TestONNX run the onnx's backend tests against all registered operatos
 func TestONNX(t *testing.T) {
 	for optype := range operators {
+		optype := optype
 		for _, tc := range testbackend.GetOpTypeTests(optype) {
-			tc := tc // capture range variable
-			t.Run(tc().GetInfo(), tc().RunTest(NewGraph(), false))
+			tc := tc() // capture range variable
+			t.Run(tc.GetInfo(), tc.RunTest(NewGraph(), true))
 		}
 	}
 }
