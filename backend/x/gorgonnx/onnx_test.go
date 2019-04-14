840b5e4c39959cb65c609fa856dcef7e4d27a55d
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-04-14 09:06:56 +0200

    feat: new operator factory

---
+++ backend/x/gorgonnx/onnx_test.go
@@ -0,0 +1,18 @@
+package gorgonnx
+
+import (
+	"testing"
+
+	"github.com/owulveryck/onnx-go/backend/testbackend"
+	_ "github.com/owulveryck/onnx-go/backend/testbackend/onnx"
+)
+
+// TestONNX run the onnx's backend tests against all registered operatos
+func TestONNX(t *testing.T) {
+	for optype := range operators {
+		for _, tc := range testbackend.GetOpTypeTests(optype) {
+			tc := tc // capture range variable
+			t.Run(tc().GetInfo(), tc().RunTest(NewGraph(), false))
+		}
+	}
+}
---
+++ backend/x/gorgonnx/operator.go
@@ -0,0 +1,38 @@
+package gorgonnx
+
+import "fmt"
+
+func init() {
+	//operators = make(map[string]operator, 0)
+}
+
+func register(optype string, op operator) {
+	operators[optype] = op
+}
+
+var operators = map[string]operator{}
+
+type operator interface {
+	// apply analyse the graph to find the children of the node
+	// the extract its gorgonia.Node references and assign the result of the operation
+	// to the node n
+	apply(g *Graph, n *Node) error
+}
+
+// check conditions of the children.
+// It returns an error is:
+//  * children's length != arity
+//  * if at least one of the children's pointer fo gorgoniaNode is nil
+func checkCondition(children []*Node, arity int) error {
+	if len(children) != arity {
+		return fmt.Errorf("bad arity for add operation (have %v, want %v)", len(children), arity)
+	}
+	// fail fast
+	for i := range children {
+		if children[i].gorgoniaNode == nil {
+			return fmt.Errorf("at least one of the children node is nil")
+		}
+	}
+
+	return nil
+}
