40aef2306a2255ab03de0f0c02b5d28aab29b80f
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-04-15 14:00:01 +0200

    wip

--- backend/x/gorgonnx/onnx_test.go
+++ backend/x/gorgonnx/onnx_test.go
@@ -1,19 +1,45 @@
 package gorgonnx
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/owulveryck/onnx-go/backend/testbackend"
 	_ "github.com/owulveryck/onnx-go/backend/testbackend/onnx"
 )
 
+type report struct {
+	info    string
+	failed  bool
+	skipped bool
+}
+
 // TestONNX run the onnx's backend tests against all registered operatos
 func TestONNX(t *testing.T) {
 	for optype := range operators {
 		optype := optype
 		for _, tc := range testbackend.GetOpTypeTests(optype) {
-			tc := tc() // capture range variable
+			tc := *tc() // capture range variable
 			t.Run(tc.GetInfo(), tc.RunTest(NewGraph(), true))
 		}
 	}
+}
+
+func TestONNXCoverage(t *testing.T) {
+	if _, ok := os.LookupEnv("ONNX_COVERAGE"); !ok {
+		t.SkipNow()
+	}
+	status := make([]report, 0)
+	for _, tc := range testbackend.GetAllRegisteredTests() {
+		tc := *tc() // capture range variable
+		t.Run(tc.GetInfo(), tc.RunTest(NewGraph(), true))
+		status = append(status, report{tc.GetInfo(), t.Failed(), t.Skipped()})
+	}
+	for i := range status {
+		status := status[i]
+		fmt.Printf("|%-40v|%-10v|%-10v|\n", status.info, status.skipped, status.failed)
+
+	}
+
 }
