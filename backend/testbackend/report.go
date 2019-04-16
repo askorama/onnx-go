fd114faa4aeedd896178a9078add2b96ca89510e
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-04-16 13:19:34 +0200

    feat: sort by status

--- backend/testbackend/report.go
+++ backend/testbackend/report.go
@@ -2,12 +2,14 @@ package testbackend
 
 import (
 	"io"
+	"sort"
 	"text/template"
 )
 
 // WriteCoverageReport on w
 func WriteCoverageReport(w io.Writer, test []*TestCase) {
 	t := template.Must(template.New("report").Parse(reportTemplate))
+	sort.Sort(byStatus(test))
 	t.Execute(w, test)
 }
 
@@ -32,3 +34,18 @@ const reportTemplate = `
 {{define "green"}}![#c5f015](https://placehold.it/15/c5f015/000000?text=+){{ end}}
 {{define "orange"}}![#ffa500](https://placehold.it/15/FFA500/000000?text=+){{end}}
 `
+
+type byStatus []*TestCase
+
+func (a byStatus) Len() int      { return len(a) }
+func (a byStatus) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a byStatus) Less(i, j int) bool {
+	switch {
+	case a[i].Failed && !a[j].Failed:
+		return true
+	case !a[i].Failed && a[j].Skipped:
+		return true
+	default:
+		return false
+	}
+}
