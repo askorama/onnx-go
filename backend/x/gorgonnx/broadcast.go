9a40401471098794c1e4178db14bef3efc3ca8b5
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-04-18 17:37:12 +0200

    feat: add broadcast

--- backend/x/gorgonnx/broadcast.go
+++ backend/x/gorgonnx/broadcast.go
@@ -15,15 +15,37 @@ func broadcast(a, b *Node) (*gorgonia.Node, *gorgonia.Node, error) {
 	// TODO find a smarter way to achieve this
 	switch {
 	case len(a.gorgoniaNode.Shape()) == 1 && len(b.gorgoniaNode.Shape()) != 1:
-		//TODO
-		return a.gorgoniaNode, b.gorgoniaNode, &onnx.ErrNotImplemented{
-			Message: fmt.Sprintf("broadcast not yet implemented for shape %v, %v", a.gorgoniaNode.Shape(), b.gorgoniaNode.Shape()),
+		// Make an educated guess: find the axis that has the same dimension
+		bShape := b.gorgoniaNode.Shape()
+		dims := make([]int, len(bShape))
+		for i := 0; i < len(bShape); i++ {
+			dims[i] = 1
+			if bShape[i] == a.gorgoniaNode.Shape()[0] {
+				dims[i] = bShape[i]
+			}
 		}
+		// Reshape node a
+		aR, err := gorgonia.Reshape(a.gorgoniaNode, dims)
+		if err != nil {
+			return nil, nil, err
+		}
+		return gorgonia.Broadcast(aR, b.gorgoniaNode, getBroadcastPattern(aR, b.gorgoniaNode))
 	case len(a.gorgoniaNode.Shape()) != 1 && len(b.gorgoniaNode.Shape()) == 1:
-		//TODO
-		return a.gorgoniaNode, b.gorgoniaNode, &onnx.ErrNotImplemented{
-			Message: fmt.Sprintf("broadcast not yet implemented for shape %v, %v", a.gorgoniaNode.Shape(), b.gorgoniaNode.Shape()),
+		// Make an educated guess: find the axis that has the same dimension
+		aShape := a.gorgoniaNode.Shape()
+		dims := make([]int, len(aShape))
+		for i := 0; i < len(aShape); i++ {
+			dims[i] = 1
+			if aShape[i] == b.gorgoniaNode.Shape()[0] {
+				dims[i] = aShape[i]
+			}
 		}
+		// Reshape node a
+		bR, err := gorgonia.Reshape(b.gorgoniaNode, dims)
+		if err != nil {
+			return nil, nil, err
+		}
+		return gorgonia.Broadcast(a.gorgoniaNode, bR, getBroadcastPattern(a.gorgoniaNode, bR))
 	case len(a.gorgoniaNode.Shape()) == 3 && len(b.gorgoniaNode.Shape()) == 4:
 		// Reshape node a
 		aR, err := gorgonia.Reshape(a.gorgoniaNode, append([]int{1}, a.gorgoniaNode.Shape()...))
