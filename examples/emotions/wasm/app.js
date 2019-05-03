21512f6fd8815784b35282c66d4fb39428e828cb
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-05-03 09:32:26 +0200

    wip: wasm demo for emotions

--- examples/emotions/wasm/app.js
+++ examples/emotions/wasm/app.js
@@ -10,5 +10,5 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 }
 
 document.getElementById('smile').addEventListener('click', () => {
-    context.drawImage(video, 0, 0, 640, 480);
+    context.drawImage(video, 0, 0, 64, 64);
 });
