bdf8511768996c0ad9577d43bd2c216c82bd53ba
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-05-08 14:14:48 +0200

    wip

--- doc/introduction/onnx-demo.go
+++ doc/introduction/onnx-demo.go
@@ -3,18 +3,42 @@
 package main
 
 import (
-	"io"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
-	"os"
 )
 
 func init() {
 	http.HandleFunc("/image", imagePostHandler)
-	http.HandleFunc("/model", imagePostHandler)
+	http.HandleFunc("/model", modelPostHandler)
+}
+
+type img struct {
+	Image interface{}
+	Bla   string `json:"bla"`
 }
 
 func imagePostHandler(w http.ResponseWriter, r *http.Request) {
-	io.Copy(os.Stdout, r.Body)
+	dec := json.NewDecoder(r.Body)
+	var payload img
+	err := dec.Decode(&payload)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+	fmt.Fprintf(w, `{result: "ok"}`)
 }
 func modelPostHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
 }
