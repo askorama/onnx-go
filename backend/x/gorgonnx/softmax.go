0189c12b12598b565a5f83fcadce7caa639a652d
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-05-16 10:50:12 +0200

    chore: refactoring

--- backend/x/gorgonnx/softmax.go
+++ backend/x/gorgonnx/softmax.go
@@ -22,55 +22,16 @@ func (s *softmax) apply(g *Graph, n *Node) error {
 		return err
 	}
 	a := children[0].gorgoniaNode
-	var reshaped *gorgonia.Node
-	if len(a.Shape()) > 2 {
-		if s.axis > len(a.Shape()) {
-			return errors.New("softmax cannot be applied on an axis > len(shape()) of the input")
-		}
-		row := 1
-		col := 1
-		for i, shape := range a.Shape() {
-			if i < s.axis {
-				row = row * shape
-			} else {
-				col = col * shape
-			}
-		}
-		reshaped, err = gorgonia.Reshape(a, []int{row, col})
-		if err != nil {
-			return err
-		}
-	} else {
-		reshaped = a
-	}
-	var output *gorgonia.Node
-	ax := 1
-	if reshaped.Shape()[0] == 1 {
-		ax = 0
-	}
-
-	m1, err := gorgonia.Max(reshaped, ax)
+	coerced, err := coerceInto2D(a, s.axis)
 	if err != nil {
 		return err
 	}
-
-	if reshaped.Shape()[0] == 1 {
-		output, err = gorgonia.Sub(reshaped, m1)
-		if err != nil {
-			return err
-		}
-	} else {
-		a1, b1, err := gorgonia.Broadcast(reshaped, m1, gorgonia.NewBroadcastPattern(nil, []byte{1}))
-		if err != nil {
-			return err
-		}
-		output, err = gorgonia.Sub(a1, b1)
-		if err != nil {
-			return err
-		}
+	stableCoerced, err := stabilizeNode(coerced)
+	if err != nil {
+		return err
 	}
 	var exp, sum *gorgonia.Node
-	if exp, err = gorgonia.Exp(output); err == nil {
+	if exp, err = gorgonia.Exp(stableCoerced); err == nil {
 		axis := 1
 		if exp.IsScalar() {
 			axis = 0
@@ -90,6 +51,54 @@ func (s *softmax) apply(g *Graph, n *Node) error {
 		return err
 	}
 	return err
+}
+
+// coerceInto2D does a reshape of the node following this rule:
+// For an arbitrary n-dimensional tensor input \in [a_0, a_1, ..., a_{k-1}, a_k, ..., a_{n-1}]
+// and k is the axis provided, then input will be coerced into a 2-dimensional tensor
+// with dimensions [a_0 * ... * a_{k-1}, a_k * ... * a_{n-1}].
+// For the default case where axis=1,
+// this means the input tensor will be coerced into a 2D tensor of dimensions [a_0, a_1 * ... * a_{n-1}],
+// where a_0 is often the batch size.
+// In this situation, we must have a_0 = N and a_1 * ... * a_{n-1} = D.
+// Each of these dimensions must be matched correctly, or else the operator will throw errors.
+func coerceInto2D(n *gorgonia.Node, k int) (*gorgonia.Node, error) {
+	if len(n.Shape()) == 2 {
+		return n, nil
+	}
+	if k > len(n.Shape()) {
+		return nil, errors.New("softmax cannot be applied on an axis > len(shape()) of the input")
+	}
+	row := 1
+	col := 1
+	for i, shape := range n.Shape() {
+		if i < k {
+			row = row * shape
+		} else {
+			col = col * shape
+		}
+	}
+	return gorgonia.Reshape(n, []int{row, col})
+}
+
+// stabilize a node consits of substracting the max value from every row
+// this function returns an error is the input is not a tensor or a matrix
+func stabilizeNode(n *gorgonia.Node) (*gorgonia.Node, error) {
+	switch {
+	case len(n.Shape()) == 2:
+		m1, err := gorgonia.Max(n, 1)
+		if err != nil {
+			return nil, err
+		}
+
+		a1, b1, err := gorgonia.Broadcast(n, m1, gorgonia.NewBroadcastPattern(nil, []byte{1}))
+		if err != nil {
+			return nil, err
+		}
+		return gorgonia.Sub(a1, b1)
+	default:
+		return nil, errors.New("can only stabilize a vector or a matrix")
+	}
 }
 
 func (s *softmax) init(o onnx.Operation) error {
