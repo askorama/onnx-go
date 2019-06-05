6f9df0258b460ebe257a6041bec3a072c13fd747
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-06-05 11:29:28 +0200

    feat: initial implementation of SAME_UPPER padding

--- backend/x/gorgonnx/maxpool.go
+++ backend/x/gorgonnx/maxpool.go
@@ -1,6 +1,8 @@
 package gorgonnx
 
 import (
+	"errors"
+
 	"github.com/owulveryck/onnx-go"
 	"gorgonia.org/gorgonia"
 	"gorgonia.org/tensor"
@@ -20,9 +22,14 @@ func newMaxpool() operator {
 //    https://godoc.org/gorgonia.org/gorgonia#MaxPool2D
 // test with go test -run=TestONNX/maxpool
 type maxpool struct {
+	padding     string
 	pad         []int
 	stride      []int
 	kernelShape tensor.Shape
+}
+
+func ceilDivInt(a, b int) int {
+	return (a + b - 1) / b
 }
 
 func (c *maxpool) apply(g *Graph, n *Node) error {
@@ -30,6 +37,17 @@ func (c *maxpool) apply(g *Graph, n *Node) error {
 	err := checkCondition(children, 1)
 	if err != nil {
 		return err
+	}
+	x := children[0].gorgoniaNode
+	switch c.padding {
+	case "SAME_UPPER":
+		outputSpatialShape := make([]int, len(x.Shape()[2:]))
+		for i, v := range x.Shape()[2:] {
+			outputSpatialShape[i] = ceilDivInt(v, c.stride[i])
+			// pad_shape[i] = (output_spatial_shape[i] - 1) * strides_spatial_shape[i] + kernel_spatial_shape[i] - input_spatial_shape[i]
+			c.pad[i] = (outputSpatialShape[i]-1)*c.stride[i] + c.kernelShape[i] - v
+		}
+	default:
 	}
 	n.gorgoniaNode, err = gorgonia.MaxPool2D(
 		children[0].gorgoniaNode,
@@ -41,11 +59,22 @@ func (c *maxpool) apply(g *Graph, n *Node) error {
 }
 
 func (c *maxpool) init(o onnx.Operation) error {
-	autoPad, ok := o.Attributes["auto_pad"]
-	if ok && autoPad.(string) != "NOTSET" {
+	var autoPad string
+	if a, ok := o.Attributes["auto_pad"]; ok {
+		if autoPad, ok = a.(string); !ok {
+			return errors.New("autopad is not a string")
+		}
+
+	}
+	switch autoPad {
+	case "NOTSET":
+	case "SAME_UPPER":
+		c.padding = autoPad
+	case "":
+	default:
 		return &onnx.ErrNotImplemented{
 			Operator: "maxpool",
-			Message:  "auto_pad " + autoPad.(string) + " not implemented",
+			Message:  "auto_pad " + autoPad + " not implemented",
 		}
 	}
 	kernelShape, ok := o.Attributes["kernel_shape"]
@@ -102,6 +131,13 @@ func (c *maxpool) init(o onnx.Operation) error {
 					c.stride[i] = int(stride[i])
 				}
 			}
+		}
+	}
+	_, ok = o.Attributes["ceil_mode"]
+	if ok {
+		return &onnx.ErrNotImplemented{
+			Operator: "maxpool",
+			Message:  "ceil_mode not implemented",
 		}
 	}
 	_, ok = o.Attributes["dilations"]
