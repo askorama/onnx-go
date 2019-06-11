a34fb4cb5f1d1d26417a70168c31d939cbe3879c
Olivier Wulveryck <owulveryck@users.noreply.github.com>
2019-06-11 14:03:40 +0200

    Model zoo bench (#84)
    * feat: test the model from the zoo
    
    * feat: benchmark for the execution
    
    * feat: complete test
    
    * chore: Add a makefile for testing all the models
    
    * chore: ignore errors and test *all* models

--- examples/model_zoo_executor/README.md
+++ examples/model_zoo_executor/README.md
@@ -14,3 +14,31 @@ The utility evaluates the model and check if the computed output is equal to the
 If the result is ok, it displays the result:
 
 `[975.67035 -618.7244 6574.5684 668.0278 -917.27057 -1671.6357 -1952.7606 -61.54949 -777.17645 -1439.5311]`
+
+# Test files
+
+You can test (and bench a model) with `go test`.
+Simply download a model from the zoo into a subdirectory of the `./models` dir; then run:
+
+`MODELDIR=./models/YOURMODEL go test`
+
+If you have several models inside the `./models` dir, you can run them all by typing `make`
+
+## Bench
+
+A simple benchmark that measures the execution time of the model is also included; to run it:
+
+`MODELDIR=./models/YOURMODEL go test -bench=.`
+
+### Profiling
+
+You can also use this benchmark for profiling the application:
+
+`MODELDIR=models/YOURMODEL go test -bench=. -benchmem -memprofile memprofile.out -cpuprofile profile.out -benchtime=10s`
+
+The use the `pprof tool`:
+
+`go tool pprof -http=localhost:3999 memprofile.out`
+or 
+`go tool pprof -http=localhost:3999 profile.out`
+
---
+++ examples/model_zoo_executor/main_test.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/owulveryck/onnx-go"
+	"github.com/owulveryck/onnx-go/backend"
+	"github.com/owulveryck/onnx-go/backend/x/gorgonnx"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	model, input, output []byte
+)
+
+func TestMain(m *testing.M) {
+	dir, ok := os.LookupEnv("MODELDIR")
+	if !ok {
+		log.Println("$MODELDIR undefined")
+		os.Exit(0)
+	}
+	modelFile := filepath.Join(dir, "model.onnx")
+	var err error
+	model, err = ioutil.ReadFile(modelFile)
+	if err != nil {
+		log.Println(err)
+		os.Exit(0)
+	}
+	inputFile := filepath.Join(dir, "test_data_set_0/input_0.pb")
+	input, err = ioutil.ReadFile(inputFile)
+	if err != nil {
+		log.Println(err)
+		os.Exit(0)
+	}
+	outputFile := filepath.Join(dir, "test_data_set_0/output_0.pb")
+	output, err = ioutil.ReadFile(outputFile)
+	if err != nil {
+		log.Println(err)
+		os.Exit(0)
+	}
+	os.Exit(m.Run())
+}
+
+func testUnmarshal(tb testing.TB) (backend.ComputationBackend, *onnx.Model) {
+	// Create a backend receiver
+	engine := gorgonnx.NewGraph()
+	// Create a model and set the execution backend
+	m := onnx.NewModel(engine)
+	// Decode it into the model
+	err := m.UnmarshalBinary(model)
+	if err != nil {
+		tb.Fatal(err)
+	}
+	return engine, m
+}
+
+func testSetInput(tb testing.TB, m *onnx.Model) {
+	inputT, err := onnx.NewTensor(input)
+	if err != nil {
+		tb.Fatal(err)
+	}
+	m.SetInput(0, inputT)
+}
+
+func testRun(tb testing.TB, engine backend.ComputationBackend) {
+	err := engine.Run()
+	if err != nil {
+		tb.Fatal(err)
+	}
+}
+
+func TestModel(t *testing.T) {
+	var engine backend.ComputationBackend
+	var m *onnx.Model
+	t.Run("Unmarshal", func(t *testing.T) {
+		engine, m = testUnmarshal(t)
+	})
+	t.Run("Set input", func(t *testing.T) {
+		testSetInput(t, m)
+	})
+	t.Run("Run", func(t *testing.T) {
+		testRun(t, engine)
+	})
+	t.Run("Validate output", func(t *testing.T) {
+		computedOutputT, err := m.GetOutputTensors()
+		if err != nil {
+			t.Fatal(err)
+		}
+		outputT, err := onnx.NewTensor(output)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.InDeltaSlice(t, outputT.Data(), computedOutputT[0].Data(), 5e-3, "the two tensors should be equal.")
+	})
+}
+
+func BenchmarkRun(b *testing.B) {
+	engine, m := testUnmarshal(b)
+	testSetInput(b, m)
+	for n := 0; n < b.N; n++ {
+		testRun(b, engine)
+	}
+}
