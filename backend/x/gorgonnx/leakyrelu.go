39122e5860297dfb23f09194dc885e1fb58f73fa
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-06-12 08:26:13 +0200

    feat: more efficient way to leakyrelu

--- backend/x/gorgonnx/leakyrelu.go
+++ backend/x/gorgonnx/leakyrelu.go
@@ -1,14 +1,92 @@
 package gorgonnx
 
 import (
+	"encoding/binary"
 	"errors"
+	"hash"
+	"hash/fnv"
 
+	"github.com/chewxy/hm"
 	"github.com/owulveryck/onnx-go"
 	"gorgonia.org/gorgonia"
+	"gorgonia.org/tensor"
 )
 
 type leakyRELU struct {
 	alpha float32
+}
+
+func (l *leakyRELU) Arity() int {
+	return 1
+}
+
+func (l *leakyRELU) Type() hm.Type {
+	a := hm.TypeVariable('a')
+	return hm.NewFnType(a, a)
+}
+
+func (l *leakyRELU) InferShape(inputs ...gorgonia.DimSizer) (tensor.Shape, error) {
+	if inputs[0] == nil {
+		return nil, errors.New("learyRELU: infershape failed, nil shape")
+	}
+
+	return inputs[0].(tensor.Shape), nil
+}
+
+func (l *leakyRELU) Do(inputs ...gorgonia.Value) (gorgonia.Value, error) {
+	if len(inputs) != l.Arity() {
+		return nil, errors.New("leakyrelu: wrong number of arguments")
+	}
+	t, ok := inputs[0].(*tensor.Dense)
+	if !ok {
+		return nil, errors.New("leakyrelu: only dense are supporter")
+
+	}
+	if vals, ok := t.Data().([]float64); ok {
+		for i := range vals {
+			if vals[i] >= 0 {
+				continue
+			}
+			vals[i] *= float64(l.alpha)
+		}
+	}
+	if vals, ok := t.Data().([]float32); ok {
+		for i := range vals {
+			if vals[i] >= 0 {
+				continue
+			}
+			vals[i] *= l.alpha
+		}
+	}
+	return t, nil
+}
+
+func (l *leakyRELU) ReturnsPtr() bool {
+	return true
+}
+
+func (l *leakyRELU) CallsExtern() bool {
+	return false
+}
+
+func (l *leakyRELU) OverwritesInput() int {
+	return 0
+}
+
+func (l *leakyRELU) WriteHash(h hash.Hash) {
+	if err := binary.Write(h, binary.LittleEndian, []byte(`learyRELU`)); err != nil {
+		panic(err)
+	}
+}
+
+func (l *leakyRELU) Hashcode() uint32 {
+	h := fnv.New32a()
+	l.WriteHash(h)
+	return h.Sum32()
+}
+
+func (l *leakyRELU) String() string {
+	return "leakyRELU"
 }
 
 func init() {
@@ -26,7 +104,8 @@ func (l *leakyRELU) apply(g *Graph, n *Node) error {
 		nodes[i] = children[i].gorgoniaNode
 	}
 	var err error
-	n.gorgoniaNode, err = gorgonia.LeakyRelu(nodes[0], float64(l.alpha))
+	n.gorgoniaNode, err = gorgonia.ApplyOp(l, nodes[0])
+	//n.gorgoniaNode, err = gorgonia.LeakyRelu(nodes[0], float64(l.alpha))
 	return err
 }
 
