0cb1a6d91a3432345f7b31f6a338a7478e6d805e
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-06-13 09:00:52 +0200

    feat: handling float64

--- backend/x/gorgonnx/batchnorm_op.go
+++ backend/x/gorgonnx/batchnorm_op.go
@@ -52,9 +52,6 @@ func (b *batchNorm) Do(values ...gorgonia.Value) (gorgonia.Value, error) {
 	if x.Shape()[0] != 1 {
 		return nil, errors.New("batchNorm_op expects a BCHW tensor with B=1")
 	}
-	if x.Dtype() != tensor.Float32 {
-		panic("batchnorm_op: dense not []float32 not implemented")
-	}
 	if len(b.scale.Shape()) != 1 || len(b.bias.Shape()) != 1 ||
 		len(b.mean.Shape()) != 1 || len(b.varN.Shape()) != 1 {
 		return nil, errors.New("batchnorp_op only support 1 dim scale,bias,mean and var")
@@ -79,24 +76,47 @@ func (b *batchNorm) Do(values ...gorgonia.Value) (gorgonia.Value, error) {
 	}()
 	// TODO: handle float64
 
-	vals, err := native.Tensor3F32(x)
-	if err != nil {
-		return nil, err
-	}
-	// xNorm = (x - meanN) / sqrt( varN + b.epsilon)
-	// output = scaleN * xNorm + biasN
-	for c := 0; c < len(vals); c++ {
-		mean := b.mean.Data().([]float32)[c]
-		varV := b.varN.Data().([]float32)[c]
-		scale := b.scale.Data().([]float32)[c]
-		bias := b.bias.Data().([]float32)[c]
-		for h := 0; h < len(vals[c]); h++ {
-			for w := 0; w < len(vals[c][h]); w++ {
-				x := vals[c][h][w]
-				//outT3[c][h][w] = scale*((x-mean)/sqrtF32(varV+b.epsilon)) + bias
-				vals[c][h][w] = scale*((x-mean)/sqrtF32(varV+b.epsilon)) + bias
+	switch {
+	case x.Dtype() == tensor.Float32:
+		vals, err := native.Tensor3F32(x)
+		if err != nil {
+			return nil, err
+		}
+		// xNorm = (x - meanN) / sqrt( varN + b.epsilon)
+		// output = scaleN * xNorm + biasN
+		for c := 0; c < len(vals); c++ {
+			mean := b.mean.Data().([]float32)[c]
+			varV := b.varN.Data().([]float32)[c]
+			scale := b.scale.Data().([]float32)[c]
+			bias := b.bias.Data().([]float32)[c]
+			for h := 0; h < len(vals[c]); h++ {
+				for w := 0; w < len(vals[c][h]); w++ {
+					x := vals[c][h][w]
+					vals[c][h][w] = scale*((x-mean)/sqrtF32(varV+b.epsilon)) + bias
+				}
 			}
 		}
+	case x.Dtype() == tensor.Float64:
+		vals, err := native.Tensor3F64(x)
+		if err != nil {
+			return nil, err
+		}
+		// xNorm = (x - meanN) / sqrt( varN + b.epsilon)
+		// output = scaleN * xNorm + biasN
+		for c := 0; c < len(vals); c++ {
+			mean := b.mean.Data().([]float64)[c]
+			varV := b.varN.Data().([]float64)[c]
+			scale := b.scale.Data().([]float64)[c]
+			bias := b.bias.Data().([]float64)[c]
+			for h := 0; h < len(vals[c]); h++ {
+				for w := 0; w < len(vals[c][h]); w++ {
+					x := vals[c][h][w]
+					vals[c][h][w] = scale*((x-mean)/math.Sqrt(varV+float64(b.epsilon))) + bias
+				}
+			}
+		}
+	default:
+		return x, errors.New("type not handled")
 	}
 	return x, nil
 }
--- backend/x/gorgonnx/batchnorm_test.go
+++ backend/x/gorgonnx/batchnorm_test.go
@@ -8,7 +8,7 @@ import (
 	"gorgonia.org/tensor"
 )
 
-func TestBatchnorm(t *testing.T) {
+func TestBatchnorm_float32(t *testing.T) {
 	xT := tensor.New(
 		tensor.WithShape(1, 2, 1, 3),
 		tensor.WithBacking([]float32{-1, 0, 1, 2, 3, 4}),
@@ -36,6 +36,71 @@ func TestBatchnorm(t *testing.T) {
 	expectedT := tensor.New(
 		tensor.WithShape(1, 2, 1, 3),
 		tensor.WithBacking([]float32{-0.999995, 0, 0.999995, -0.22474074, 1, 2.2247407}),
+	)
+
+	g := gorgonia.NewGraph()
+	xN := gorgonia.NodeFromAny(g, xT, gorgonia.WithName("x"))
+	scaleN := gorgonia.NodeFromAny(g, scaleT, gorgonia.WithName("scale"))
+	biasN := gorgonia.NodeFromAny(g, biasT, gorgonia.WithName("bias"))
+	meanN := gorgonia.NodeFromAny(g, meanT, gorgonia.WithName("mean"))
+	varN := gorgonia.NodeFromAny(g, varT, gorgonia.WithName("var"))
+	batchNormOp := &batchNorm{
+		scale:   scaleN.Value(),
+		bias:    biasN.Value(),
+		mean:    meanN.Value(),
+		varN:    varN.Value(),
+		epsilon: 1e-5,
+	}
+
+	output, err := gorgonia.ApplyOp(batchNormOp, xN)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	m := gorgonia.NewTapeMachine(g)
+	err = m.RunAll()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(output.Shape()) != len(expectedT.Shape()) {
+		t.Fatalf("wrong dimension, got %v, expect %v", output.Shape(), expectedT.Shape())
+	}
+	for i := range output.Shape() {
+		if output.Shape()[i] != expectedT.Shape()[i] {
+			t.Fatalf("wrong dimension, got %v, expect %v", output.Shape(), expectedT.Shape())
+		}
+	}
+	assert.InDeltaSlice(t, output.Value().Data(), expectedT.Data(), 1e-6)
+}
+func TestBatchnorm_float64(t *testing.T) {
+	xT := tensor.New(
+		tensor.WithShape(1, 2, 1, 3),
+		tensor.WithBacking([]float64{-1, 0, 1, 2, 3, 4}),
+	)
+
+	scaleT := tensor.New(
+		tensor.WithShape(2),
+		tensor.WithBacking([]float64{1, 1.5}),
+	)
+
+	biasT := tensor.New(
+		tensor.WithShape(2),
+		tensor.WithBacking([]float64{0, 1}),
+	)
+
+	meanT := tensor.New(
+		tensor.WithShape(2),
+		tensor.WithBacking([]float64{0, 3}),
+	)
+
+	varT := tensor.New(
+		tensor.WithShape(2),
+		tensor.WithBacking([]float64{1, 1.5}),
+	)
+	expectedT := tensor.New(
+		tensor.WithShape(1, 2, 1, 3),
+		tensor.WithBacking([]float64{-0.999995, 0, 0.999995, -0.22474074, 1, 2.2247407}),
 	)
 
 	g := gorgonia.NewGraph()
