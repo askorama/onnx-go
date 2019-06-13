d1844bd310955a153da53497d24ab1fb8e51049b
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-06-13 16:43:42 +0200

    wip: processing output of tiny yolo v2

--- examples/tiny_yolov2/main.go
+++ examples/tiny_yolov2/main.go
@@ -2,28 +2,48 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"image"
+	"image/draw"
 	"image/jpeg"
 	"image/png"
 	"io/ioutil"
 	"log"
+	"math"
 	"os"
+	"sort"
 
 	"github.com/owulveryck/onnx-go"
 	"github.com/owulveryck/onnx-go/backend/x/gorgonnx"
 	"github.com/owulveryck/onnx-go/internal/x/images"
 	"gorgonia.org/tensor"
+	"gorgonia.org/tensor/native"
+)
+
+// The 416x416 image is divided into a 13x13 grid. Each of these grid cells
+// will predict 5 bounding boxes (boxesPerCell). A bounding box consists of
+// five data items: x, y, width, height, and a confidence score. Each grid
+// cell also predicts which class each bounding box belongs to.
+//
+const (
+	h, w         = 416, 416
+	blockSize    = 32
+	gridHeight   = 13
+	gridWidth    = 13
+	boxesPerCell = 5
+	numClasses   = 20
 )
 
 var (
 	model   = flag.String("model", "model.onnx", "path to the model file")
-	inputT  = flag.String("inputT", "", "path of an input tensor for testing")
 	imgF    = flag.String("img", "", "path of an input tensor for testing")
+	inputT  = flag.String("input", "", "tensor")
 	img     image.Image
 	classes = []string{"aeroplane", "bicycle", "bird", "boat", "bottle",
 		"bus", "car", "cat", "chair", "cow",
 		"diningtable", "dog", "horse", "motorbike", "person",
 		"pottedplant", "sheep", "sofa", "train", "tv/monitor"}
+	anchors = []float32{1.08, 1.19, 3.42, 4.41, 6.63, 11.38, 9.42, 5.11, 16.62, 10.52}
 )
 
 func main() {
@@ -56,27 +76,22 @@ func main() {
 }
 
 func getInput() tensor.Tensor {
-	switch {
-	case *inputT != "":
-		if _, err := os.Stat(*inputT); err != nil && os.IsNotExist(err) {
-			log.Fatalf("%v does not exist", *inputT)
-		}
+	if *inputT != "" {
 		b, err := ioutil.ReadFile(*inputT)
 		if err != nil {
 			log.Fatal(err)
 		}
-		inputT, err := onnx.NewTensor(b)
+		t, err := onnx.NewTensor(b)
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Println(inputT)
-		img, err = images.TensorToImg(inputT)
+		img, err = images.TensorToImg(t)
 		if err != nil {
-			log.Println(err)
+			log.Fatal(err)
 		}
-		return inputT
-
-	case *imgF != "":
+		return t
+	}
+	if *imgF != "" {
 		f, err := os.Open(*imgF)
 		if err != nil {
 			log.Fatal(err)
@@ -86,15 +101,14 @@ func getInput() tensor.Tensor {
 		if err != nil {
 			log.Fatal(err)
 		}
-		inputT := tensor.New(tensor.WithShape(1, 3, 416, 416), tensor.Of(tensor.Float32))
+		inputT := tensor.New(tensor.WithShape(1, 3, h, w), tensor.Of(tensor.Float32))
 		err = images.ImageToBCHW(img, inputT)
 		if err != nil {
 			log.Fatal(err)
 		}
 		return inputT
-	default:
-		log.Fatal("Please speficy an input")
 	}
+	log.Fatal("Please speficy an input")
 	return nil
 }
 
@@ -102,13 +116,63 @@ func processOutput(t []tensor.Tensor, err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(t[0].Data())
-	f, err := os.Create("output.png")
+	dense := t[0].(*tensor.Dense)
+	s := make([]int, 3)
+	copy(s, dense.Shape()[1:])
+	// Ignore the first dimension
+	err = dense.Reshape(s...)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if err := png.Encode(f, img); err != nil {
+	features, err := native.Tensor3F32(dense)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var classification = make([]box, 13*13*5)
+	var counter int
+	for b := 0; b < len(features); b += 25 {
+		for cx := 0; cx < len(features[b]); cx++ {
+			for cy := 0; cy < len(features[b][cx]); cy++ {
+				bb := (b + 1) / 25
+				classification[counter] = box{
+					gridcell:    []int{cx, cy},
+					boundindBox: b,
+					// The predicted tx and ty coordinates are relative to the location
+					// of the grid cell; we use the logistic sigmoid to constrain these
+					// coordinates to the range 0 - 1. Then we add the cell coordinates
+					// (0-12) and multiply by the number of pixels per grid cell (32).
+					// Now x and y represent center of the bounding box in the original
+					// 416x416 image space.
+					// https://github.com/hollance/Forge/blob/04109c856237faec87deecb55126d4a20fa4f59b/Examples/YOLO/YOLO/YOLO.swift#L154
+					x: (float32(cx) + sigmoid(features[b][cx][cy])) * blockSize,
+					y: (float32(cy) + sigmoid(features[b+1][cx][cy])) * blockSize,
+					// The size of the bounding box, tw and th, is predicted relative to
+					// the size of an "anchor" box. Here we also transform the width and
+					// height into the original 416x416 image space.
+					w:          exp(features[b+2][cx][cy]) * anchors[2*bb] * blockSize,
+					h:          exp(features[b+3][cx][cy]) * anchors[2*bb+1] * blockSize,
+					confidence: sigmoid(features[b+4][cx][cy]),
+					classes:    features[b+5 : b+24][cx][cy],
+				}
+				counter++
+			}
+		}
+	}
+	sort.Sort(sort.Reverse(byConfidence(classification)))
+	//sort.Sort(sort.Reverse(byGridCell(classification)))
+	for _, e := range classification[:6] {
+		fmt.Println(e)
+	}
+	f, err := os.Create("output.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	m := image.NewRGBA(img.Bounds())
+
+	draw.Draw(m, m.Bounds(), img, image.ZP, draw.Src)
+
+	if err := png.Encode(f, m); err != nil {
 		f.Close()
 		log.Fatal(err)
 	}
@@ -123,4 +187,31 @@ func must(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+type box struct {
+	gridcell               []int
+	boundindBox            int
+	x, y, w, h, confidence float32
+	classes                []float32
+}
+
+type byGridCell []box
+type byConfidence []box
+
+func (a byGridCell) Len() int      { return len(a) }
+func (a byGridCell) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a byGridCell) Less(i, j int) bool {
+	return a[i].gridcell[0] < a[j].gridcell[0] || a[i].gridcell[1] < a[j].gridcell[1]
+}
+
+func (a byConfidence) Len() int           { return len(a) }
+func (a byConfidence) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byConfidence) Less(i, j int) bool { return a[i].confidence < a[j].confidence }
+
+func sigmoid(sum float32) float32 {
+	return float32(1.0 / (1.0 + math.Exp(float64(-sum))))
+}
+func exp(val float32) float32 {
+	return float32(math.Exp(float64(val)))
 }
