2b468a9ddf03def9e69850549b8c7dd891d8f34b
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-06-17 21:49:23 +0200

    feat: drawibg rectangle on the picture, but result is still weird

--- examples/tiny_yolov2/main.go
+++ examples/tiny_yolov2/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"image"
+	"image/color"
 	"image/draw"
 	"image/jpeg"
 	"image/png"
@@ -129,34 +130,34 @@ func processOutput(t []tensor.Tensor, err error) {
 	for b := 0; b < len(data)-25; b += 25 {
 		for cx := 0; cx < gridWidth; cx++ {
 			for cy := 0; cy < gridHeight; cy++ {
-				tx := data[b][cx][cy]
-				ty := data[b+1][cx][cy]
-				tw := data[b+2][cx][cy]
-				th := data[b+3][cx][cy]
-				tc := data[b+4][cx][cy]
-				tclasses := data[b+5 : b+24][cx][cy]
+				element := (b + 1) / 25
+				tx := data[b][cy][cx]
+				ty := data[b+1][cy][cx]
+				tw := data[b+2][cy][cx]
+				th := data[b+3][cy][cx]
+				tc := data[b+4][cy][cx]
+				tclasses := data[b+5 : b+24][cy][cx]
+				// The predicted tx and ty coordinates are relative to the location
+				// of the grid cell; we use the logistic sigmoid to constrain these
+				// coordinates to the range 0 - 1. Then we add the cell coordinates
+				// (0-12) and multiply by the number of pixels per grid cell (32).
+				// Now x and y represent center of the bounding box in the original
+				// 416x416 image space.
+				// https://github.com/hollance/Forge/blob/04109c856237faec87deecb55126d4a20fa4f59b/Examples/YOLO/YOLO/YOLO.swift#L154
+				x := int((float32(cx) + sigmoid(tx)) * blockSize)
+				y := int((float32(cy) + sigmoid(ty)) * blockSize)
+				// The size of the bounding box, tw and th, is predicted relative to
+				// the size of an "anchor" box. Here we also transform the width and
+				// height into the original 416x416 image space.
+				w := int(exp(tw) * anchors[2*element] * blockSize)
+				h := int(exp(th) * anchors[2*element+1] * blockSize)
 
 				classification[counter] = box{
 					gridcell:    []int{cx, cy},
+					r:           image.Rect(max(0, x-w/2), max(0, y-h/2), min(416-1, x+w/2), min(416, y+h/2)),
 					boundindBox: b,
-					// The predicted tx and ty coordinates are relative to the location
-					// of the grid cell; we use the logistic sigmoid to constrain these
-					// coordinates to the range 0 - 1. Then we add the cell coordinates
-					// (0-12) and multiply by the number of pixels per grid cell (32).
-					// Now x and y represent center of the bounding box in the original
-					// 416x416 image space.
-					// https://github.com/hollance/Forge/blob/04109c856237faec87deecb55126d4a20fa4f59b/Examples/YOLO/YOLO/YOLO.swift#L154
-					x: (float32(cx) + sigmoid(tx)) * blockSize,
-					y: (float32(cy) + sigmoid(ty)) * blockSize,
-					// The size of the bounding box, tw and th, is predicted relative to
-					// the size of an "anchor" box. Here we also transform the width and
-					// height into the original 416x416 image space.
-					//w:          exp(tw) * anchors[2*b] * blockSize,
-					//h:          exp(th) * anchors[2*b+1] * blockSize,
-					w:          tw, // TODO use the anchor
-					h:          th, // TODO use the anchor
-					confidence: sigmoid64(tc),
-					classes:    getOrderedElements(softmax(tclasses)),
+					confidence:  sigmoid64(tc),
+					classes:     getOrderedElements(softmax(tclasses)),
 				}
 				counter++
 			}
@@ -164,18 +165,19 @@ func processOutput(t []tensor.Tensor, err error) {
 	}
 	sort.Sort(sort.Reverse(byConfidence(classification)))
 	//sort.Sort(sort.Reverse(byGridCell(classification)))
-	for _, e := range classification {
-		if e.confidence > 0.30 {
-			fmt.Printf("at %vx%v with confidence %2.2f%%: %v\n", e.x, e.y, e.confidence, e.classes[:3])
-		}
-	}
+	printClassification(classification)
 	f, err := os.Create("output.png")
 	if err != nil {
 		log.Fatal(err)
 	}
-	m := image.NewRGBA(img.Bounds())
+	m := image.NewNRGBA(img.Bounds())
 
 	draw.Draw(m, m.Bounds(), img, image.ZP, draw.Src)
+	for _, c := range classification {
+		if c.confidence > 0.30 {
+			drawRectangle(m, c.r)
+		}
+	}
 
 	if err := png.Encode(f, m); err != nil {
 		f.Close()
@@ -184,6 +186,25 @@ func processOutput(t []tensor.Tensor, err error) {
 
 	if err := f.Close(); err != nil {
 		log.Fatal(err)
+	}
+
+}
+
+func printClassification(classification []box) {
+	var classes []element
+	for _, e := range classification {
+		if e.confidence > 0.30 {
+			if e.classes[0].prob > 0.30 {
+				classes = append(classes, e.classes...)
+				fmt.Printf("at %v with confidence %2.2f%%: %v\n", e.r, e.confidence, e.classes[:3])
+			}
+		}
+	}
+	sort.Sort(sort.Reverse(byProba(classes)))
+	for _, e := range classes {
+		if e.prob > 0.4 {
+			fmt.Println(e)
+		}
 	}
 
 }
@@ -197,10 +218,16 @@ func must(err error) {
 type box struct {
 	gridcell    []int
 	boundindBox int
-	x, y, w, h  float32
+	r           image.Rectangle
 	confidence  float64
 	classes     []element
 }
+
+type byProba []element
+
+func (a byProba) Len() int           { return len(a) }
+func (a byProba) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byProba) Less(i, j int) bool { return a[i].prob < a[j].prob }
 
 type byGridCell []box
 type byConfidence []box
@@ -225,30 +252,25 @@ func exp(val float32) float32 {
 	return float32(math.Exp(float64(val)))
 }
 
-func softmax(a []float32) []float32 {
-	max := -float32(math.MaxFloat32)
-	for _, v := range a {
-		if v > max {
-			max = v
-		}
-	}
-	output := make([]float32, len(a))
-	var sum float32
+func softmax(a []float32) []float64 {
+	var sum float64
+	output := make([]float64, len(a))
 	for i := 0; i < len(a); i++ {
-		sum += float32(math.Exp(float64(a[i] - max)))
+		output[i] = math.Exp(float64(a[i]))
+		sum += output[i]
 	}
-	for i := 0; i < len(a); i++ {
-		output[i] = float32(math.Exp(float64(a[i]-max))) / sum
+	for i := 0; i < len(output); i++ {
+		output[i] = output[i] / sum
 	}
 	return output
 }
 
 type element struct {
-	prob  float32
+	prob  float64
 	class string
 }
 
-func getOrderedElements(input []float32) []element {
+func getOrderedElements(input []float64) []element {
 	elems := make([]element, len(input))
 	for i := 0; i < len(elems); i++ {
 		elems[i] = element{
@@ -265,3 +287,48 @@ type elements []element
 func (a elements) Len() int           { return len(a) }
 func (a elements) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a elements) Less(i, j int) bool { return a[i].prob < a[j].prob }
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+//func drawRectangle(img image.Image, r image.Rectangle) *image.NRGBA {
+func drawRectangle(img *image.NRGBA, r image.Rectangle) {
+	col := color.RGBA{255, 0, 0, 255} // Red
+
+	//dst := image.NewNRGBA(img.Bounds())
+	//draw.Draw(dst, dst.Bounds(), img, image.ZP, draw.Src)
+	//draw.Draw(dst, img.Bounds(), img, image.ZP, draw.Over)
+	// HLine draws a horizontal line
+	hLine := func(x1, y, x2 int) {
+		for ; x1 <= x2; x1++ {
+			img.Set(x1, y, col)
+		}
+	}
+
+	// VLine draws a veritcal line
+	vLine := func(x, y1, y2 int) {
+		for ; y1 <= y2; y1++ {
+			img.Set(x, y1, col)
+		}
+	}
+
+	// Rect draws a rectangle utilizing HLine() and VLine()
+	rect := func(r image.Rectangle) {
+		hLine(r.Min.X, r.Max.Y, r.Max.X)
+		hLine(r.Min.X, r.Min.Y, r.Max.X)
+		vLine(r.Max.X, r.Min.Y, r.Max.Y)
+		vLine(r.Min.X, r.Min.Y, r.Max.Y)
+	}
+	rect(r)
+	//return dst
+}
