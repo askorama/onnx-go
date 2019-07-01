ebf20cdbf228165d3bbcfe98e8036e7724a800b9
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-07-01 22:39:32 +0200

    feat(wip): new implementation for gemm
    benchmark           old ns/op     new ns/op     delta
    BenchmarkGemm-4     424890        133088        -68.68%
    
    benchmark           old allocs     new allocs     delta
    BenchmarkGemm-4     1619           319            -80.30%
    
    benchmark           old bytes     new bytes     delta
    BenchmarkGemm-4     61402         13720         -77.66%

--- backend/x/gorgonnx/gemm.go
+++ backend/x/gorgonnx/gemm.go
@@ -2,10 +2,16 @@ package gorgonnx
 
 import (
 	"errors"
+	"fmt"
+	"hash"
+	"hash/fnv"
 
-	"github.com/google/uuid"
+	"github.com/chewxy/hm"
 	"github.com/owulveryck/onnx-go"
+	"gonum.org/v1/gonum/blas"
+	"gonum.org/v1/gonum/blas/blas32"
 	"gorgonia.org/gorgonia"
+	"gorgonia.org/tensor"
 )
 
 func init() {
@@ -28,6 +34,134 @@ type gemm struct {
 	transB bool    // Whether B should be transposed
 }
 
+func (o *gemm) Arity() int {
+	return 3
+}
+
+func (o *gemm) Type() hm.Type {
+	t := gorgonia.TensorType{Dims: 2, Of: hm.TypeVariable('a')}
+	return hm.NewFnType(t, t, t, t)
+}
+
+func (o *gemm) InferShape(inputs ...gorgonia.DimSizer) (tensor.Shape, error) {
+	// output is (m,n)
+	m, err := inputs[0].DimSize(0)
+	if err != nil {
+		return nil, err
+	}
+	if o.transA {
+		m, err = inputs[0].DimSize(1)
+		if err != nil {
+			return nil, err
+		}
+	}
+	n, err := inputs[1].DimSize(1)
+	if err != nil {
+		return nil, err
+	}
+	if o.transA {
+		n, err = inputs[1].DimSize(0)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return []int{m, n}, nil
+}
+
+func (o *gemm) Do(inputs ...gorgonia.Value) (gorgonia.Value, error) {
+	var a, b, c tensor.Tensor
+	var ok bool
+	if a, ok = inputs[0].(tensor.Tensor); !ok {
+		return nil, errors.New("gemm: not a tensor")
+	}
+	if b, ok = inputs[1].(tensor.Tensor); !ok {
+		return nil, errors.New("gemm: not a tensor")
+	}
+	if c, ok = inputs[2].(tensor.Tensor); !ok {
+		return nil, errors.New("gemm: not a tensor")
+	}
+	s, err := o.InferShape(a.Shape(), b.Shape(), c.Shape())
+	if err != nil {
+		return nil, err
+	}
+	m := s[0]
+	n := s[1]
+	if c.DataSize() != m*n {
+		backend := make([]float32, m*n)
+		switch c.DataSize() {
+		case 1:
+			for i := 0; i < len(backend); i++ {
+				backend[i] = c.Data().([]float32)[0]
+			}
+		case m:
+			for i := 0; i < n; i++ {
+				copy(backend[i:i+m], c.Data().([]float32))
+			}
+		default:
+			return nil, errors.New("Gemm: unhandled shape for C")
+		}
+		c = tensor.New(tensor.WithShape(m, n), tensor.WithBacking(backend))
+	}
+	transA := blas.NoTrans
+	transB := blas.NoTrans
+	if o.transA {
+		transA = blas.Trans
+	}
+	if o.transB {
+		transB = blas.Trans
+	}
+	// do we need to broadcast?
+	blas32.Gemm(transA, transB, o.alpha,
+		blas32.General{
+			Rows:   a.Shape()[0],
+			Cols:   a.Shape()[1],
+			Stride: a.Strides()[0],
+			Data:   a.Data().([]float32),
+		},
+		blas32.General{
+			Rows:   b.Shape()[0],
+			Cols:   b.Shape()[1],
+			Stride: b.Strides()[0],
+			Data:   b.Data().([]float32),
+		},
+		o.beta,
+		blas32.General{
+			Rows:   c.Shape()[0],
+			Cols:   c.Shape()[1],
+			Stride: c.Strides()[0],
+			Data:   c.Data().([]float32),
+		})
+
+	return c, nil
+
+}
+
+func (*gemm) ReturnsPtr() bool {
+	return true
+}
+
+func (*gemm) CallsExtern() bool {
+	return false
+}
+
+func (*gemm) OverwritesInput() int {
+	return 2
+}
+
+func (o *gemm) WriteHash(h hash.Hash) {
+	fmt.Fprintf(h, "gemm-%v-%v-%v-%v", o.transA, o.transB, o.alpha, o.beta)
+}
+
+func (o *gemm) Hashcode() uint32 {
+	h := fnv.New32a()
+	o.WriteHash(h)
+	return h.Sum32()
+}
+
+func (o *gemm) String() string {
+	return fmt.Sprintf("gemm-%v-%v-%v-%v", o.transA, o.transB, o.alpha, o.beta)
+}
+
 // Compute Y = alpha * A' * B' + beta * C, where
 //  * input tensor A has shape (M, K) or (K, M),
 //  * input tensor B has shape (K, N) or (N, K),
@@ -39,7 +173,7 @@ type gemm struct {
 // (tensor C should be unidirectional broadcastable to tensor A * B);
 //
 // https://github.com/onnx/onnx/blob/master/docs/Operators.md#Gemm
-func (m *gemm) apply(g *Graph, n *Node) error {
+func (o *gemm) apply(g *Graph, n *Node) error {
 	children := getOrderedChildren(g.g, n)
 	err := checkCondition(children, 3)
 	if err != nil {
@@ -48,91 +182,40 @@ func (m *gemm) apply(g *Graph, n *Node) error {
 	a := children[0].gorgoniaNode
 	b := children[1].gorgoniaNode
 	c := children[2].gorgoniaNode
-	if m.transA {
-		if a, err = gorgonia.Transpose(a); err != nil {
-			return err
-		}
-	}
-	if m.transB {
-		if b, err = gorgonia.Transpose(b); err != nil {
-			return err
-		}
-	}
-	if len(a.Shape()) != 2 || len(b.Shape()) != 2 || len(c.Shape()) != 2 {
-		return errors.New("gemm: input should be matrices")
-	}
-	//dimM := a.Shape()[0]
-	//dimN := b.Shape()[1]
-	ab, err := gorgonia.Mul(a, b)
-	if err != nil {
-		return err
-	}
-	alphaN := gorgonia.NewScalar(g.exprgraph, ab.Dtype(), gorgonia.WithValue(m.alpha), gorgonia.WithName("gemmAlpha"+uuid.New().String()))
-	alphaB, abB, err := gorgonia.Broadcast(alphaN, ab, gorgonia.NewBroadcastPattern([]byte{0, 1}, nil))
-	if err != nil {
-		return err
-	}
-	alphaAB, err := gorgonia.HadamardProd(alphaB, abB)
-	if err != nil {
-		return err
-	}
-	betaN := gorgonia.NewScalar(g.exprgraph, ab.Dtype(), gorgonia.WithValue(m.beta), gorgonia.WithName("gemmbeta"+uuid.New().String()))
-	betaB, cB, err := gorgonia.Broadcast(betaN, c, gorgonia.NewBroadcastPattern([]byte{0, 1}, nil))
-	if err != nil {
-		return err
-	}
-	betaC, err := gorgonia.HadamardProd(betaB, cB)
-	if err != nil {
-		return err
-	}
-	if alphaAB.Shape()[0] == betaC.Shape()[0] && alphaAB.Shape()[1] == betaC.Shape()[1] {
-		n.gorgoniaNode, err = gorgonia.Add(alphaAB, betaC)
-		if err != nil {
-			return err
-		}
-	} else {
-		alphaABB, betaCB, err := ggnBroadcast(alphaAB, betaC)
-		if err != nil {
-			return err
-		}
-		n.gorgoniaNode, err = gorgonia.Add(alphaABB, betaCB)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+	n.gorgoniaNode, err = gorgonia.ApplyOp(o, a, b, c)
+	return err
 }
 
-func (m *gemm) init(o onnx.Operation) error {
-	m.alpha = 1.0
-	m.beta = 1.0
-	if alpha, ok := o.Attributes["alpha"]; ok {
+func (o *gemm) init(op onnx.Operation) error {
+	o.alpha = 1.0
+	o.beta = 1.0
+	if alpha, ok := op.Attributes["alpha"]; ok {
 		if alpha, ok := alpha.(float32); ok {
-			m.alpha = alpha
+			o.alpha = alpha
 		} else {
 			return errors.New("Gemm: alpha is not a float32")
 		}
 	}
-	if beta, ok := o.Attributes["beta"]; ok {
+	if beta, ok := op.Attributes["beta"]; ok {
 		if beta, ok := beta.(float32); ok {
-			m.beta = beta
+			o.beta = beta
 		} else {
 			return errors.New("Gemm: beta is not a float32")
 		}
 	}
-	if transA, ok := o.Attributes["transA"]; ok {
+	if transA, ok := op.Attributes["transA"]; ok {
 		if transA, ok := transA.(int64); ok {
 			if transA == 1 {
-				m.transA = true
+				o.transA = true
 			}
 		} else {
 			return errors.New("Gemm: transA is not an int")
 		}
 	}
-	if transB, ok := o.Attributes["transB"]; ok {
+	if transB, ok := op.Attributes["transB"]; ok {
 		if transB, ok := transB.(int64); ok {
 			if transB == 1 {
-				m.transB = true
+				o.transB = true
 			}
 		} else {
 			return errors.New("Gemm: transB is not an int")
