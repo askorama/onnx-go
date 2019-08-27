33fc3bd9dcf73ec6000b4801765198a123ffe812
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-08-27 21:10:19 +0200

    feat: ExprGraph's population is ok for ops with several output

--- backend/x/gorgonnx/graph_walk.go
+++ backend/x/gorgonnx/graph_walk.go
@@ -10,38 +10,42 @@ import (
 
 // populateExprgraph by walking through the graph
 func (g *Graph) populateExprgraph() error {
+	if len(g.groups) == 0 {
+		return errors.New("cannot populate the graphi because ApplyOperation have not been called")
+	}
+
 	// Walk the graph
 	itN := g.Nodes()
-	nodes := make([]*Node, 0, itN.Len())
 	for itN.Next() {
 		// if the node is a "tensor", set it!
 		n := itN.Node().(*Node)
 		if n.t != nil && n.gorgoniaNode == nil && n.operation == nil {
 			n.gorgoniaNode = gorgonia.NodeFromAny(g.exprgraph, n.t, gorgonia.WithName(getUniqNodeName("node")))
-		} else {
-			nodes = append(nodes, n)
 		}
 	}
+	nodes := make([][]*Node, len(g.groups))
+	copy(nodes, g.groups)
 	for len(nodes) > 0 {
 		initialLen := len(nodes)
 		for i := 0; i < len(nodes); i++ {
-			n := nodes[i]
-			if n.operation != nil {
+			nilChild := false
+			for _, n := range nodes[i] {
+				//if n.operation != nil {
 				children := getOrderedChildren(g.g, n)
-				nilChild := false
 				for j := 0; j < len(children); j++ {
 					if children[j].gorgoniaNode == nil {
 						nilChild = true
 						break
 					}
 				}
-				if nilChild {
-					continue
-				}
-				err := g.applyOperation(n)
-				if err != nil {
-					return err
-				}
+				//}
+			}
+			if nilChild {
+				continue
+			}
+			err := g.applyOperation(nodes[i]...)
+			if err != nil {
+				return err
 			}
 			nodes = append(nodes[:i], nodes[i+1:]...)
 		}
@@ -53,23 +57,23 @@ func (g *Graph) populateExprgraph() error {
 }
 
 // applyOperation creates a new node on the exprgraph
-func (g *Graph) applyOperation(n *Node) error {
+func (g *Graph) applyOperation(n ...*Node) error {
 	// Is this node already in the ExprGraph?
-	if n.gorgoniaNode != nil {
+	if n[0].gorgoniaNode != nil {
 		return fmt.Errorf("unsupported case: node is already in the exprgraph")
 	}
 	var op operator
 	var opC func() operator
 	var ok bool
-	if opC, ok = operators[n.operation.Name]; !ok {
+	if opC, ok = operators[n[0].operation.Name]; !ok {
 		return &onnx.ErrNotImplemented{
-			Operator: n.operation.Name,
+			Operator: n[0].operation.Name,
 		}
 	}
 	op = opC()
-	err := op.init(*n.operation)
+	err := op.init(*n[0].operation)
 	if err != nil {
 		return err
 	}
-	return op.apply(g, n)
+	return op.apply(g, n...)
 }
