0726170c6e98938bcad262704d5ef6fb43946a64
Bohdan Ivashko <Bogdannuch@gmail.com>
2019-10-03 13:40:28 +0300

    Update attributes_test.go

--- attributes_test.go
+++ attributes_test.go
@@ -6,7 +6,6 @@ import (
 	pb "github.com/owulveryck/onnx-go/internal/pb-onnx"
 	
 	"gotest.tools/assert"
-	is "gotest.tools/assert/cmp"
 )
 
 
@@ -61,44 +60,56 @@ func TestToOperationAttributes(t *testing.T) {
 		t.Fatal(err)
 	}
 	
-	v, ok := attrs["strings"]
-	assert.Assert(t, ok)
-	v, ok = v.([]string)
-	assert.Assert(t, ok)
-	expected := []string{"a", "b"}
-	for i, v := range v {
-		assert.Check(t, expected[i] == v)
+	{
+		v, ok := attrs["strings"]
+		assert.Assert(t, ok)
+		v, ok = v.([]string)
+		assert.Assert(t, ok)
+		expected := []string{"a", "b"}
+		for i, v := range v {
+			assert.Check(t, expected[i] == v)
+		}
 	}
 	
-	v, ok = attrs["ints"]
-	assert.Assert(t, ok)
-	v, ok = v.([]int64)
-	assert.Assert(t, ok)
-	expected = []int64{1, 2}
-	for i, v := range v {
-		assert.Check(t, expected[i] == v)
+	{
+		v, ok := attrs["ints"]
+		assert.Assert(t, ok)
+		v, ok = v.([]int64)
+		assert.Assert(t, ok)
+		expected = []int64{1, 2}
+		for i, v := range v {
+			assert.Check(t, expected[i] == v)
+		}
 	}
 	
-	v, ok = attrs["floats"]
-	assert.Assert(t, ok)
-	v, ok = v.([]float32)
-	assert.Assert(t, ok)
-	expected = []float32{1, 2}
-	for i, v := range v {
-		assert.Check(t, expected[i] == v)
+	{
+		v, ok = attrs["floats"]
+		assert.Assert(t, ok)
+		v, ok = v.([]float32)
+		assert.Assert(t, ok)
+		expected = []float32{1, 2}
+		for i, v := range v {
+			assert.Check(t, expected[i] == v)
+		}
 	}
 	
-	v, ok = attrs["float"]
-	assert.Assert(t, ok)
-	assert.Check(t, v.(float32) == float32(1))
+	{
+		v, ok = attrs["float"]
+		assert.Assert(t, ok)
+		assert.Check(t, v.(float32) == float32(1))
+	}
 	
-	v, ok = attrs["int"]
-	assert.Assert(t, ok)
-	assert.Check(t, v.(int64) == int64(1))
+	{
+		v, ok = attrs["int"]
+		assert.Assert(t, ok)
+		assert.Check(t, v.(int64) == int64(1))
+	}
 	
-	v, ok = attrs["string"]
-	assert.Assert(t, ok)
-	assert.Check(t, v.(string) != "a")
+	{
+		v, ok = attrs["string"]
+		assert.Assert(t, ok)
+		assert.Check(t, v.(string) != "a")
+	}
 }
 
 func TestToOperationAttributes_NotImplemented(t *testing.T) {
@@ -108,26 +119,26 @@ func TestToOperationAttributes_NotImplemented(t *testing.T) {
 		},
 	})
 	_, ok := err.(*ErrNotImplemented)
-	t.Check(ok)
+	assert.Check(t, ok)
 	_, err = toOperationAttributes([]*pb.AttributeProto{
 		{
 			Type: pb.AttributeProto_TENSORS,
 		},
 	})
 	_, ok = err.(*ErrNotImplemented)
-	t.Check(ok)
+	assert.Check(t, ok)
 	_, err = toOperationAttributes([]*pb.AttributeProto{
 		{
 			Type: pb.AttributeProto_GRAPHS,
 		},
 	})
 	_, ok = err.(*ErrNotImplemented)
-	t.Check(ok)
+	assert.Check(t, ok)
 	_, err = toOperationAttributes([]*pb.AttributeProto{
 		{
 			Type: pb.AttributeProto_AttributeType(-1),
 		},
 	})
 	_, ok = err.(*ErrNotImplemented)
-	t.Check(ok)
+	assert.Check(t, ok)
 }
