c819ff2c42db2e5bf89a0aba0c6d37a0fb1003fd
Bohdan Ivashko <Bogdannuch@gmail.com>
2019-10-03 12:25:38 +0300

    Reduce cyclomatic complexity of attributes_test
    use asserts and checks instead of a bunch of ifs

--- attributes_test.go
+++ attributes_test.go
@@ -6,6 +6,8 @@ import (
 	pb "github.com/owulveryck/onnx-go/internal/pb-onnx"
 )
 
+
+
 func TestToOperationAttributes(t *testing.T) {
 	/*
 	 AttributeProto_UNDEFINED AttributeProto_AttributeType = 0
@@ -55,69 +57,45 @@ func TestToOperationAttributes(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if v, ok := attrs["strings"]; ok {
-		if v, ok := v.([]string); ok {
-			expected := []string{"a", "b"}
-			for i, v := range v {
-				if expected[i] != v {
-					t.Fail()
-				}
-			}
-		} else {
-			t.Fail()
-		}
-	} else {
-		t.Fail()
+	
+	v, ok := attrs["strings"]
+	t.Assert(ok)
+	v, ok := v.([]string)
+	t.Assert(ok)
+	expected := []string{"a", "b"}
+	for i, v := range v {
+		t.Check(expected[i] == v)
 	}
-	if v, ok := attrs["ints"]; ok {
-		if v, ok := v.([]int64); ok {
-			expected := []int64{1, 2}
-			for i, v := range v {
-				if expected[i] != v {
-					t.Fail()
-				}
-			}
-		} else {
-			t.Fail()
-		}
-	} else {
-		t.Fail()
+	
+	v, ok := attrs["ints"]
+	t.Assert(ok)
+	v, ok := v.([]int64)
+	t.Assert(ok)
+	expected := []int64{1, 2}
+	for i, v := range v {
+		t.Check(expected[i] == v)
 	}
-	if v, ok := attrs["floats"]; ok {
-		if v, ok := v.([]float32); ok {
-			expected := []float32{1, 2}
-			for i, v := range v {
-				if expected[i] != v {
-					t.Fail()
-				}
-			}
-		} else {
-			t.Fail()
-		}
-	} else {
-		t.Fail()
+	
+	v, ok := attrs["floats"]
+	t.Assert(ok)
+	v, ok := v.([]float32)
+	t.Assert(ok)
+	expected := []float32{1, 2}
+	for i, v := range v {
+		t.Check(expected[i] == v)
 	}
-	if v, ok := attrs["float"]; ok {
-		if v.(float32) != float32(1) {
-			t.Fail()
-		}
-	} else {
-		t.Fail()
-	}
-	if v, ok := attrs["int"]; ok {
-		if v.(int64) != int64(1) {
-			t.Fatal("bad value for int")
-		}
-	} else {
-		t.Fail()
-	}
-	if v, ok := attrs["string"]; ok {
-		if v.(string) != "a" {
-			t.Fatal("bad value for sting")
-		}
-	} else {
-		t.Fail()
-	}
+	
+	v, ok := attrs["float"]
+	t.Assert(ok)
+	t.Check(v.(float32) == float32(1))
+	
+	v, ok := attrs["int"]
+	t.Assert(ok)
+	t.Check(v.(int64) == int64(1))
+	
+	v, ok := attrs["string"]
+	t.Assert(ok)
+	t.Check(v.(string) != "a")
 }
 
 func TestToOperationAttributes_NotImplemented(t *testing.T) {
@@ -127,34 +105,26 @@ func TestToOperationAttributes_NotImplemented(t *testing.T) {
 		},
 	})
 	_, ok := err.(*ErrNotImplemented)
-	if !ok {
-		t.Fail()
-	}
+	t.Check(ok)
 	_, err = toOperationAttributes([]*pb.AttributeProto{
 		{
 			Type: pb.AttributeProto_TENSORS,
 		},
 	})
 	_, ok = err.(*ErrNotImplemented)
-	if !ok {
-		t.Fail()
-	}
+	t.Check(ok)
 	_, err = toOperationAttributes([]*pb.AttributeProto{
 		{
 			Type: pb.AttributeProto_GRAPHS,
 		},
 	})
 	_, ok = err.(*ErrNotImplemented)
-	if !ok {
-		t.Fail()
-	}
+	t.Check(ok)
 	_, err = toOperationAttributes([]*pb.AttributeProto{
 		{
 			Type: pb.AttributeProto_AttributeType(-1),
 		},
 	})
 	_, ok = err.(*ErrNotImplemented)
-	if !ok {
-		t.Fail()
-	}
+	t.Check(ok)
 }
